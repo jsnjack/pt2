8fb6e8714abf4d8a99c152990aa428feefbdce05
Yauhen Shulitski <jsnjack@gmail.com>
2024-01-17 23:22:26 +0100

    :m: Order items in list by title

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,27 +1,17 @@
-import { onMounted } from 'vue';
-
 <template>
-  <div
-    class="medium-height middle-align center-align padding"
-    style="background-color: var(--surface-container-lowest)"
-    v-if="Object.keys(props.items).length === 0"
-  >
+  <div class="medium-height middle-align center-align padding" style="background-color: var(--surface-container-lowest)"
+    v-if="Object.keys(props.items).length === 0">
     <div class="center-align">
       <i class="extra">orders</i>
       <h5>You have no items to track</h5>
     </div>
   </div>
-  <Item
-    v-for="(item, key) in props.items"
-    :key="key"
-    :item="item"
-    :itemKey="key"
-  />
+  <Item v-for="item in orderedItemsList" :key="item.key" :item="item" :itemKey="item.key" />
 </template>
 
 <script setup>
 import Item from "./Item.vue";
-import { defineProps } from "vue";
+import { defineProps, computed } from "vue";
 
 const props = defineProps({
   items: {
@@ -29,6 +19,18 @@ const props = defineProps({
     required: true,
   },
 });
+
+const orderedItemsList = computed(() => {
+  let orderedItems = [];
+  for (let key in props.items) {
+    orderedItems.push(props.items[key]);
+    orderedItems[orderedItems.length - 1].key = key;
+  }
+  return orderedItems.sort((a, b) => {
+    return a.title.localeCompare(b.title);
+  });
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
